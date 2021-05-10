869718e08f686cfeaa31cfe07c610199b5a8baf3
Jánosi József-Hunor <janosi_h@yahoo.com>
2021-05-10 12:51:44 +0300

    asd

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @BeforeAll
+    public static void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void validateSomething() {
+
+    }
+}
